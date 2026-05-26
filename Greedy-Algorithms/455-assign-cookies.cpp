b1c9c97226e02fbf25097246b44c416a8566c69e
Aman Shrivastava <amvn.303@gmail.com>
2026-05-26 13:53:22 +0530

    changes

--- Greedy-Algorithms/455-assign-cookies.cpp
+++ Greedy-Algorithms/455-assign-cookies.cpp
@@ -1,17 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
-        int i = 0, j = 0, cnt = 0;
-        while(i < g.size() && j < s.size()){
-            if(g[i] <= s[j]){
+
+        int i = 0, j = 0;
+        int cnt = 0;
+
+        while (i < g.size() && j < s.size()) {
+            if (g[i] <= s[j]) {
+                cnt++;
                 i++;
                 j++;
-                cnt++;
+            } else {
+                j++;
             }
-            else j++;
         }
+
         return cnt;
     }
 };
